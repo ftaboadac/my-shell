0098c90247a331c51563152b3d225aeaa7f407c0
Facundo Taboada <ftaboadacuria@gmail.com>
2026-05-13 16:37:47 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ def main():
                         path_to_file = os.path.join(folder,parts[1])
                         if os.path.exists(path_to_file) and os.access(path_to_file, os.X_OK):
                             print(f"{parts[1]} is {path_to_file}")
-                            false = True
+                            found = True
                             break
                 if not found:
                   print(f"{parts[1]}: not found")
