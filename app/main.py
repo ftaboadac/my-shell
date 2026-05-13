38fc017d1ac9cd99f84b4b21e78db874e71198b7
Facundo Taboada <ftaboadacuria@gmail.com>
2026-05-13 15:12:16 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,6 +7,8 @@ def main():
         command = input()
         if command == "exit":
             break
+        elif command.startswith("echo"):
+            print(command[5:])
         print(f"{command}: command not found")
 
 if __name__ == "__main__":
