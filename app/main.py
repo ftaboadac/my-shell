40127f80f38b79300bbd3098062d489e13867312
Facundo Taboada <ftaboadacuria@gmail.com>
2026-05-13 15:35:11 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,10 +5,17 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
+        commands = ['echo','exit','type']
+        parts = command.split()
         if command == "exit":
             break
         elif command.startswith("echo"):
             print(command[5:])
+        elif command.startswith("type"):
+            if len(parts) > 1 and parts[1] in commands:
+                print(f"{parts[1]} is a shell builtin")
+            else:
+                continue
         else:
             print(f"{command}: command not found")
 
