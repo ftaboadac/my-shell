9857a8055a47cfd7f1c63d558a35cd9bc788ab58
Facundo Taboada <ftaboadacuria@gmail.com>
2026-05-15 13:17:05 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -74,8 +74,8 @@ def parse(line):
     for char in line:
 
         if escape_next:
-            if (char == "\\" or char == '"') and in_double:
-                print("banana")
+            #if (char == "\\" or char == '"') and in_double:
+                
             current += char
             escape_next = False
         elif char == "'" and not in_single and not in_double:
